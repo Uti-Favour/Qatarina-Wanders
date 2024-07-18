7f9fcc6dc3fb098af276fa3acf589c54eee75077
Uti-Favour <utifavour15@gmail.com>
2024-07-18 12:09:15 +0100

    Book grid

--- pages/Books.tsx
+++ pages/Books.tsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import Navbar from '../src/components/Navbar'
 import Footer from '../src/components/Footer'
+import { all_books } from '../src/constants/booklist'
 
 const Books : React.FC = () => {
   return (
-    <section className='bg-black h-screen'>
+    <section className=' h-screen'>
       <Navbar/>
-      <Footer/>
+      <div>
+        <h1 className='flex justify-center text-5xl mt-20'>Book By Qararina Wanders</h1>
+      </div>
+<div className='grid lg:grid-cols-4 grid-cols-2 lg:mx-20 mx-5 lg:mt-36 mt-5'>
+{all_books.map((book) => (
+  <div>
+    <img src={book.imagelink} alt="" />
+  </div>
+))}
+</div>
+     <div className='bg-black '>
+     <Footer/>
+     </div>
     </section>
   )
 }
