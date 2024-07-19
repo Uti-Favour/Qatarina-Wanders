23459d3dab3114896bf6fcb753ba0f407bd4fa6f
Uti Favour <135446153+Uti-Favour@users.noreply.github.com>
2024-07-19 12:42:00 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import Books from '../pages/Books';
 
+import About from "../src/components/About";
 
 const App: React.FC = () => {
   return (
@@ -11,6 +12,9 @@ const App: React.FC = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path='/books' element={<Books/>}/>
+<Route path='/about' element={<About/>}/>
+
+
       </Routes>
     </Router>
   );
