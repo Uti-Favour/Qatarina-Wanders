84c067fd66515be06faefbd3bde5715e59d96361
Uti-Favour <utifavour15@gmail.com>
2024-07-20 06:02:53 +0100

    Added ScrollToTop

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop";
 import Home from "../pages/Home";
 import Books from "../pages/Books";
 import AboutHer from "../pages/AboutHer";
@@ -7,6 +8,7 @@ import AboutHer from "../pages/AboutHer";
 const App: React.FC = () => {
   return (
     <Router>
+      <ScrollToTop/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/books" element={<Books />} />
